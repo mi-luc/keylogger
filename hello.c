b2ba7a22dbb0fcac56a76e3293436c5defca897a
mi-luc <89728054+mi-luc@users.noreply.github.com>
2022-11-17 12:16:31 +0200

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Dumi si Luca");
+MODULE_DESCRIPTION("kernel module");
+MODULE_VERSION("1");
+static int __init init(void) {
+ printk(KERN_INFO "Hello, World!\n");
+ return 0;
+}
+static void __exit exit(void) {
+ printk(KERN_INFO "Goodbye, World!\n");
+}
+module_init(init);
+module_exit(exit);
